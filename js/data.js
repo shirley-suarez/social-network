b672eb689547dec4b60763e1a277a0c202bdfb0d
jonhks <jonathan@laboratoria.la>
2018-07-03 15:24:48 -0600

    falta desabilitar botones

--- js/data.js
+++ js/data.js
@@ -1,3 +1,12 @@
-let google   = document.getElementById('google');
+// Imputs
+const inputName = document.getElementById("icon-name");
+const inputEmail = document.getElementById('icon-email');
+const inputPassword = documet.getElementById('icon-password')
+const inputPassword2 = documetn.getElementById('icon-password-2');
 
+
+// botons
+const google   = document.getElementById('google');
+const login = document.getElementById('login');
+const singup = document.getElementById('singup');
 

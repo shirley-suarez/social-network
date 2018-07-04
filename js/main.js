ed365622c46523b310715a040bbc5e5a48943e0d
jonhks <jonathan@laboratoria.la>
2018-07-04 12:04:23 -0600

    para bajar cambios de sil v1

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ crearCuenta: (name, email, pass, ) => {
     console.log(name, email, pass, )
     // location.href='../views/singin.html'
   if (name.length !== 0 && email.length !== 0 && pass.length !== 0) {
-    alert(name, email, pass);
+    // alert(name, email, pass);
     location.href='../views/singin.html';
-    printUserNav();
     }else {
       modal();
     }
@@ -16,7 +15,4 @@ modal= () => {
 alert("rellena todos los campos");
 }
 
-printUserNav = () => {
-  let returnUser = JSON.parse(localStorage.getItem("resultado"));
-  console.log(returnUser);
-}
+

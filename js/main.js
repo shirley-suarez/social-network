10f36cb6728cdad6beab3ced7ef4d42d30e5f958
jonhks <jonathan@laboratoria.la>
2018-07-04 10:45:55 -0600

    trabajando en recuperar datos del localStorage

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ window.social= {
 crearCuenta: (name, email, pass, ) => {
     console.log(name, email, pass, )
     // location.href='../views/singin.html'
-
   if (name.length !== 0 && email.length !== 0 && pass.length !== 0) {
-    location.href='../views/content.html';
+    alert(name, email, pass);
+    location.href='../views/singin.html';
+    printUserNav();
     }else {
       modal();
     }
@@ -13,4 +14,9 @@ crearCuenta: (name, email, pass, ) => {
 
 modal= () => {
 alert("rellena todos los campos");
+}
+
+printUserNav = () => {
+  let returnUser = JSON.parse(localStorage.getItem("resultado"));
+  console.log(returnUser);
 }
